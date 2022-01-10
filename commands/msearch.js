3b1b2b271f764bdad5c8cfb616fdbf482e9b9559
KingMedieval <tshimada@purdue.edu>
2022-01-10 16:46:45 +0900

    test

--- commands/msearch.js
+++ commands/msearch.js
@@ -34,12 +34,12 @@ module.exports = {
       .setColor("#F8AA2A");
 
     try {
-      response = await fetch(`https://api.deezer.com/search?q="${encodedSearchID}"`).then((res) => {
+      result = await fetch(`https://api.deezer.com/search?q="${encodedSearchID}"`).then((res) => {
         status = res.status;
         return res.json()
       });
       for (let i = 0; i < 10; i++) {
-        resultsEmbed.addField(response.data[i].link, `${i + 1}. ${response.data[i].title} - ${response.data[i].artist.name}`)
+        resultsEmbed.addField(result.data[i].link, `${i + 1}. ${result.data[i].title} - ${result.data[i].artist.name}`)
       }
 
       let resultsMessage = await message.channel.send(resultsEmbed);
@@ -48,12 +48,35 @@ module.exports = {
         const pattern = /^[0-9]{1,2}(\s*,\s*[0-9]{1,2})*$/;
         return pattern.test(msg.content);
       }
+      message.channel.activeCollector = true;
+      const response = await message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 30000,
+        errors: ["time"]
+      });
+      const reply = response.first().content;
+
+
+      if (reply.includes(",")) {
+        let songs = reply.split(",").map((str) => str.trim());
+
+        for (let song of songs) {
+          await message.client.commands
+            .get("play")
+            .execute(message, [resultsEmbed.fields[parseInt(song) - 1].name]);
+        }
+      } else {
+        const choice = resultsEmbed.fields[parseInt(response.first()) - 1].name;
+        message.client.commands.get("play").execute(message, [choice]);
+      }
+
+      message.channel.activeCollector = false;
+      resultsMessage.delete().catch(console.error);
+      response.first().delete().catch(console.error);
     } catch (error) {
       console.error(error);
       message.channel.activeCollector = false;
       message.reply(error.message).catch(console.error);
-
-
 
     }
   }

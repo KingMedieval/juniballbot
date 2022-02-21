6daeccadb44f2b98ec3d450aa5bd7a1a57f11219
KingMedieval <tshimada@purdue.edu>
2022-02-21 13:39:51 +0900

    .

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ i18n.configure({
  */
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
-  client.user.setActivity(`${PREFIX}help and ${PREFIX}play`, { type: "LISTENING" });
+  client.user.setActivity(`${PREFIX}help|made with ❤️ by tomo`, { type: "LISTENING" });
 });
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
